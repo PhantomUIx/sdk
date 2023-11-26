e32a3a5ed0fa893f6cb9a82e622051739af5232c
Tristan Ross <tristan.ross@midstall.com>
2023-11-26 11:40:26 -0800

    fix: prevent zig segfault

--- build.zig
+++ build.zig
@@ -3,30 +3,70 @@ const std = @import("std");
 const AvailableDep = struct { []const u8, []const u8 };
 const AvailableDeps = []const AvailableDep;
 
+pub const PhantomModule = struct {
+    provides: ?Provides = null,
+    dependencies: ?[][]const u8 = null,
+
+    pub const Provides = struct {
+        scenes: ?[][]const u8 = null,
+        displays: ?[][]const u8 = null,
+
+        pub fn value(self: Provides, kind: std.meta.FieldEnum(Provides)) [][]const u8 {
+            return (switch (kind) {
+                .scenes => self.scenes,
+                .displays => self.displays,
+                else => null,
+            }) orelse &[_][]const u8{};
+        }
+
+        pub fn count(self: Provides, kind: std.meta.FieldEnum(Provides)) bool {
+            return self.value(kind).len;
+        }
+    };
+
+    pub fn getProvider(self: PhantomModule) Provides {
+        return if (self.provides) |value| value else .{};
+    }
+};
+
 pub const availableDepenencies = blk: {
     const buildDeps = @import("root").dependencies;
     var count: usize = 0;
     for (buildDeps.root_deps) |dep| {
-        if (std.mem.startsWith(u8, dep[0], "phantom.")) count += 1;
+        const pkg = @field(buildDeps.packages, dep[1]);
+        if (pkg.build_zig) |buildZig| {
+            if (@hasDecl(buildZig, "phantomModule") and @TypeOf(@field(buildZig, "phantomModule")) == PhantomModule) {
+                count += 1;
+            }
+        }
     }
 
     var i: usize = 0;
     var deps: [count]AvailableDep = undefined;
     for (buildDeps.root_deps) |dep| {
-        if (std.mem.startsWith(u8, dep[0], "phantom.")) {
-            deps[i] = dep;
-            i += 1;
+        const pkg = @field(buildDeps.packages, dep[1]);
+        if (pkg.build_zig) |buildZig| {
+            if (@hasDecl(buildZig, "phantomModule") and @TypeOf(@field(buildZig, "phantomModule")) == PhantomModule) {
+                deps[i] = dep;
+                i += 1;
+            }
         }
     }
     break :blk deps;
 };
 
-pub fn TypeFor(comptime prefix: []const u8) type {
+pub fn TypeFor(comptime kind: std.meta.FieldEnum(PhantomModule.Provides)) type {
     const buildDeps = @import("root").dependencies;
 
     var fieldCount: usize = 0;
     for (buildDeps.root_deps) |dep| {
-        if (std.mem.startsWith(u8, dep[0], "phantom." ++ prefix)) fieldCount += 1;
+        const pkg = @field(buildDeps.packages, dep[1]);
+        if (pkg.build_zig) |buildZig| {
+            if (@hasDecl(buildZig, "phantomModule") and @TypeOf(@field(buildZig, "phantomModule")) == PhantomModule) {
+                const mod = buildZig.phantomModule;
+                fieldCount += mod.getProvider().count(kind);
+            }
+        }
     }
 
     if (fieldCount == 0) {
@@ -43,13 +83,19 @@ pub fn TypeFor(comptime prefix: []const u8) type {
     var fields: [fieldCount]std.builtin.Type.EnumField = undefined;
     var i: usize = 0;
     for (buildDeps.root_deps) |dep| {
-        if (std.mem.startsWith(u8, dep[0], "phantom." ++ prefix)) {
-            fields[i] = .{
-                .name = dep[0][(("phantom." ++ prefix).len + 1)..],
-                .value = i,
-            };
+        const pkg = @field(buildDeps.packages, dep[1]);
+        if (pkg.build_zig) |buildZig| {
+            if (@hasDecl(buildZig, "phantomModule") and @TypeOf(@field(buildZig, "phantomModule")) == PhantomModule) {
+                const mod = buildZig.phantomModule;
 
-            i += 1;
+                for (mod.getProvider().value(kind)) |name| {
+                    fields[i] = .{
+                        .name = name,
+                        .value = i,
+                    };
+                    i += 1;
+                }
+            }
         }
     }
 
@@ -74,7 +120,7 @@ pub fn Dependencies() type {
 
     for (availableDepenencies, &fields, 0..) |dep, *field, i| {
         field.* = .{
-            .name = dep[0][8..dep[0].len],
+            .name = dep[0],
             .type = *std.Build.Dependency,
             .default_value = null,
             .is_comptime = false,
@@ -96,7 +142,7 @@ pub fn dependencies(b: *std.Build, args: anytype) Dependencies() {
     var self: Dependencies() = undefined;
 
     inline for (availableDepenencies) |dep| {
-        @field(self, dep[0][8..dep[0].len]) = importPkg(b, dep[0], dep[1], args);
+        @field(self, dep[0]) = importPkg(b, dep[0], dep[1], args);
     }
     return self;
 }
@@ -119,11 +165,6 @@ pub fn build(b: *std.Build) void {
     var importer_data = std.ArrayList(u8).init(b.allocator);
     defer importer_data.deinit();
 
-    importer_data.writer().print(
-        \\pub fn import(comptime {s}: type) type {{
-        \\  return struct {{
-    , .{if (availableDepenencies.len > 0) "phantom" else "_"}) catch |e| @panic(@errorName(e));
-
     // TODO: as all modules are expected to follow the same layout as core,
     // we can do a file/directory check for specific files.
     // However, there could be some exceptions...
@@ -139,7 +180,7 @@ pub fn build(b: *std.Build) void {
         inline for (availableDepenencies) |dep| {
             importer_data.writer().print(
                 \\pub usingnamespace blk: {{
-                \\    const imports = @import("{s}").import(phantom);
+                \\    const imports = @import("{s}");
             , .{dep[0]}) catch |e| @panic(@errorName(e));
 
             modSplit.reset();
@@ -181,17 +222,61 @@ pub fn build(b: *std.Build) void {
         }
     }
 
-    importer_data.writer().print(
-        \\  }};
-        \\}}
-    , .{}) catch |e| @panic(@errorName(e));
+    const phantomCore = blk: {
+        const buildDeps = @import("root").dependencies;
+        for (buildDeps.root_deps) |dep| {
+            if (std.mem.eql(u8, dep[0], "phantom")) {
+                break :blk importPkg(b, dep[0], dep[1], .{
+                    .target = target,
+                    .optimize = optimize,
+                    .no_importer = true,
+                });
+            }
+        }
+
+        @panic("Cannot find Phantom UI core");
+    };
 
     var importer_deps: [availableDepenencies.len]std.Build.ModuleDependency = undefined;
     inline for (availableDepenencies, 0..) |dep, i| {
-        const imported_dep = @field(deps, dep[0][8..dep[0].len]);
+        const pkg = @field(@import("root").dependencies.packages, dep[1]);
+        const imported_dep = @field(deps, dep[0]);
+        const origModule = imported_dep.module(dep[0]);
+
+        const depsList = std.ArrayList(std.Build.ModuleDependency).init(b.allocator);
+        errdefer depsList.deinit();
+
+        depsList.append(.{
+            .name = "phantom",
+            .module = phantomCore.module("phantom"),
+        }) catch @panic("OOM");
+
+        for (pkg.build_zig.?.phantomModule.getDependencies()) |depName| {
+            const buildDeps = @import("root").dependencies;
+            var found = false;
+            for (buildDeps.root_deps) |d| {
+                if (std.mem.eql(u8, d[0], depName)) {
+                    const depDep = importPkg(b, d[0], d[1], .{
+                        .target = target,
+                        .optimize = optimize,
+                        .no_importer = true,
+                    });
+
+                    depsList.append(depDep.module(d[0])) catch @panic("OOM");
+                    found = true;
+                    break;
+                }
+            }
+
+            if (!found) std.debug.panic("Could not find dependency {s} for {s}", .{ depName, dep[0] });
+        }
+
         importer_deps[i] = .{
             .name = dep[0],
-            .module = imported_dep.module(dep[0]),
+            .module = imported_dep.builder.createModule(.{
+                .source_file = origModule.source_file,
+                .dependencies = depsList.items,
+            }),
         };
     }
 
